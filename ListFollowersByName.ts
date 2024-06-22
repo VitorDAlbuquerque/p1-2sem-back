c92c0216d30a6507d0b9a990050063070fcb4473
Kauan Santos <85942974+Kauan-afk@users.noreply.github.com>
2024-06-21 23:01:53 -0300

    Add files via upload

---
+++ ListFollowersByName.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import { prisma } from "../../database";
+
+export class ListFollowersByName {
+    async handle(request: Request, response: Response){
+        try{
+            const {name, userId} = request.body
+
+            const user = await prisma.user.findUnique({
+                where:{
+                    id: userId
+                }
+            })
+
+            if(!user){
+                return response.status(500).send({ err: "Usuário não existe no banco" });
+            }
+
+            const followes = await prisma.isFollow.findMany({
+                where:{
+                    userFollowedId: userId,
+                    userFollowing:{
+                        name:{
+                            contains: name,
+                            mode: 'insensitive',
+                        }
+                    }
+                },
+                include:{
+                    userFollowing:{
+                        select:{
+                            name: true,
+                            imgIndex: true
+                        }
+                    }
+                }
+            })
+            
+            return response.status(200).json(followes)
+            
+        } catch{
+            return response.status(500).send("Erro, tente novamente")
+        }
+    }
+}
